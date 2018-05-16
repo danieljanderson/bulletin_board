5b652482f8295cc2596e1178b5b81723aba41c81
Daniel <danieljanderson0616@gmail.com>
2018-05-16 16:20:02 -0400

    fixed error

--- src/board.js
+++ src/board.js
@@ -1,29 +1,46 @@
 import React, {Component} from 'react'
 import Note from './note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
   constructor(props){
     super(props)
     this.state={
+      notes:[]
+    }
+    this.add = this.add.bind(this)
+    this.eachNote = this.eachNote.bind(this)
+    this.update = this.update.bind(this)
+    this.remove = this.remove.bind(this)
+    this.nextId = this.nextId.bind(this)
+  }
+  componentWillMount() {
+    var self = this
+    if(this.props.count) {
+      fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+        .then(response => response.json())
+        .then(json => json[0]
+                .split('. ')
+                .forEach(sentence => self.add(sentence.substring(0, 25))))
+    }
+  }
+  add(text){
+    this.setState(prevState=>({
       notes:[
+        ...prevState.notes,
         {
-          id:0,
-          note:"Call Lisa"
-        },
-        {
-          id:1,
-          note:"Email John"
-        },
-        {
-          id:2,
-          note:"order printer ink"
+          id:this.nextId(),
+          note:text
         }
       ]
-    }
-    this.eachNote = this.eachNote.bind(this)
-    this.update=this.update.bind(this)
-    this.remove=this.remove.bind(this)
+    }))
   }
+
+nextId(){
+  this.uniqueId=this.uniqueId || 0
+  return this.uniqueId++
+}
+
   update(newText,i){
     console.log('updating item at index',i,newText)
     this.setState(prevState=>({
@@ -55,6 +72,10 @@ render(){
   return (
     <div className="board">
       {this.state.notes.map(this.eachNote)}
+      <button onClick={this.add.bind(null,"New Note")}
+              id="add">
+            <FaPlus />
+          </button>
       </div>
   )
   }
